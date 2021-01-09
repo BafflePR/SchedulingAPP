199867a11ef0f771a2e353fa08f866a5488d4aa5
BafflePR <63696409+BafflePR@users.noreply.github.com>
2021-01-09 12:00:22 +0530

    Update app.py

--- app.py
+++ app.py
@@ -1,20 +1,27 @@
 import requests
 
-
+#getting date and time
 datetime = input('Enter current date and time in YYYY/MM/DD H:M:S format : ')
 
+#storing url
 url = f'http://127.0.0.1:8000/{datetime}/checkstatus'
 
+#sending get request
 response = requests.get(url=url)
 
+#converting the response into json
 data = response.json()
 
+#printing the response
 print(data['status'])
 
+##checking whether server is active
 url='http://127.0.0.1:8000/ping/'
 
+#sending get request
 response1 = requests.get(url=url)
 
+#converting the response into json
 server_status = response.json()
 
 print(server_status)
